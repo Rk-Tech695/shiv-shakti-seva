f352474aff965982b945dcef618d2ed792a7fc82
Prashant Kumar <prashantkumar53175@gmail.com>
2026-05-29 05:05:28 +0530

    add digital donation counter

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,156 +1,156 @@
-import prisma from '../config/prisma.js';
-
-export const adminLogin = async (req, res) => {
-
-  try {
-
-    console.log("BODY =>", req.body);
-
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const admin = await prisma.admin.findFirst({
-      where: {
-        username
-      }
-    });
-
-    console.log("ADMIN =>", admin);
-
-    if (!admin) {
-
-      return res.status(404).json({
-        success: false,
-        message: 'Admin not found'
-      });
-
-    }
-
-    if (admin.password !== password) {
-
-      return res.status(401).json({
-        success: false,
-        message: 'Wrong password'
-      });
-
-    }
-
-    return res.json({
-      success: true,
-      message: 'Login successful'
-    });
-
-  } catch (error) {
-
-    console.log("FULL ERROR =>");
-    console.log(error);
-
-    return res.status(500).json({
-      success: false,
-      message: error.message
-    });
-
-  }
-
-};
-
 // import prisma from '../config/prisma.js';
 
-// import bcrypt from 'bcryptjs';
+// export const adminLogin = async (req, res) => {
 
-// export const adminLogin =
-//   async (req, res) => {
+//   try {
 
-//     try {
+//     console.log("BODY =>", req.body);
 
-//       console.log(
-//         "BODY =>",
-//         req.body
-//       );
+//     const username = req.body.username;
+//     const password = req.body.password;
 
-//       const username =
-//         req.body.username;
-
-//       const password =
-//         req.body.password;
-
-//       const admin =
-//         await prisma.admin.findFirst({
-
-//           where: {
-
-//             username
-
-//           }
-
-//         });
-
-//       console.log(
-//         "ADMIN =>",
-//         admin
-//       );
-
-//       if (!admin) {
-
-//         return res.status(404).json({
-
-//           success: false,
-
-//           message:
-//             'Admin not found'
-
-//         });
-
+//     const admin = await prisma.admin.findFirst({
+//       where: {
+//         username
 //       }
+//     });
 
-//       const isMatch =
-//         await bcrypt.compare(
+//     console.log("ADMIN =>", admin);
 
-//           password,
+//     if (!admin) {
 
-//           admin.password
-
-//         );
-
-//       if (!isMatch) {
-
-//         return res.status(401).json({
-
-//           success: false,
-
-//           message:
-//             'Wrong password'
-
-//         });
-
-//       }
-
-//       return res.json({
-
-//         success: true,
-
-//         message:
-//           'Login successful'
-
-//       });
-
-//     } catch (error) {
-
-//       console.log(
-//         "FULL ERROR =>"
-//       );
-
-//       console.log(error);
-
-//       return res.status(500).json({
-
+//       return res.status(404).json({
 //         success: false,
-
-//         message:
-//           error.message
-
+//         message: 'Admin not found'
 //       });
 
 //     }
 
-//   };
+//     if (admin.password !== password) {
+
+//       return res.status(401).json({
+//         success: false,
+//         message: 'Wrong password'
+//       });
+
+//     }
+
+//     return res.json({
+//       success: true,
+//       message: 'Login successful'
+//     });
+
+//   } catch (error) {
+
+//     console.log("FULL ERROR =>");
+//     console.log(error);
+
+//     return res.status(500).json({
+//       success: false,
+//       message: error.message
+//     });
+
+//   }
+
+// };
+
+import prisma from '../config/prisma.js';
+
+import bcrypt from 'bcryptjs';
+
+export const adminLogin =
+  async (req, res) => {
+
+    try {
+
+      console.log(
+        "BODY =>",
+        req.body
+      );
+
+      const username =
+        req.body.username;
+
+      const password =
+        req.body.password;
+
+      const admin =
+        await prisma.admin.findFirst({
+
+          where: {
+
+            username
+
+          }
+
+        });
+
+      console.log(
+        "ADMIN =>",
+        admin
+      );
+
+      if (!admin) {
+
+        return res.status(404).json({
+
+          success: false,
+
+          message:
+            'Admin not found'
+
+        });
+
+      }
+
+      const isMatch =
+        await bcrypt.compare(
+
+          password,
+
+          admin.password
+
+        );
+
+      if (!isMatch) {
+
+        return res.status(401).json({
+
+          success: false,
+
+          message:
+            'Wrong password'
+
+        });
+
+      }
+
+      return res.json({
+
+        success: true,
+
+        message:
+          'Login successful'
+
+      });
+
+    } catch (error) {
+
+      console.log(
+        "FULL ERROR =>"
+      );
+
+      console.log(error);
+
+      return res.status(500).json({
+
+        success: false,
+
+        message:
+          error.message
+
+      });
+
+    }
+
+  };
